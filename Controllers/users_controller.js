d63d3105832b6078e410b32e06f42a1391d3da94
dero-hub <munenederrick862@gmail.com>
2022-06-16 02:24:36 +0300

    Fully implemented Users CRUD Apis

--- Controllers/users_controller.js
+++ Controllers/users_controller.js
@@ -10,3 +10,62 @@ exports.create = async (req, res) => {
         return err.message;
       }
 }
+
+exports.getOne = async (req, res) => {
+    try {
+        let id = req.params.id;
+        let data = {
+            User: await users_service.getOne(id, res)
+        }
+        return data
+    } catch (err) {
+        return err.message;
+    }
+}
+
+exports.getAll = async (req, res) => {
+    try {
+        let data = {
+            Users: await users_service.getAll(req, res)
+        }
+        return data
+    } catch (err) {
+        return err.message;
+    }
+}
+
+exports.update = async (req, res) => {
+    try {
+        let id = req.params.id;
+        let body = req.body;
+        let data = {
+            User: await users_service.update(id, body, res)
+        }
+        return data
+    } catch (err) {
+        return err.message;
+    }
+}
+
+exports.deleteOne = async (req, res) => {
+    try {
+        let id = req.params.id;
+        let data = {
+            User: await users_service.deleteOne(id, res)
+        }
+        return data
+    } catch (err) {
+        return err.message;
+    }
+}
+
+exports.deleteAll = async (req, res) => {
+    try {
+        let data = {
+            User: await users_service.deleteAll(req, res)
+        }
+        return data
+    } catch (err) {
+        return err.message;
+    }
+}
--- Services/users_service.js
+++ Services/users_service.js
@@ -8,10 +8,11 @@ const exeption = (msg) => {
     return message;
 }
 
+let msg = '';
+
 exports.create = async (req, res) => {
     try{
     const {firstname, lastname, email, phone, password} = req;
-    let msg = '';
 
     if(!(firstname || lastname || email || phone || password)){
          msg = "Please fill in all the details"
@@ -44,3 +45,94 @@ exports.create = async (req, res) => {
     res.status(500).send(err.message);
 };
 }
+
+exports.getOne = async(id, res) => {
+
+    try{
+    let user = await UsersModel.findOne({where: {id: id}});
+    let userStr = JSON.stringify(user);
+    let userPar = JSON.parse(userStr);
+
+    if(!userPar){
+        msg = "User not found!"
+        return res.status(500).json(exeption(msg));
+    }
+
+    console.log(userPar);
+
+   return res.status(200).json(userPar);
+}catch(err){
+    res.status(500).json(err.message)
+}
+}
+
+exports.getAll = async(req, res) => {
+    try {
+        let users = await UsersModel.findAll();
+        let str = JSON.stringify(users);
+        let par = JSON.parse(str)
+        
+        console.log(par);
+
+      return res.json(par);
+    } catch (err) {
+        res.status(500).json(err.message);
+    }
+}
+
+exports.update = async(id, req, res) => {
+    try {
+        let {firstname, lastname, email, phone, password} = req;
+        let existingUser = await UsersModel.findOne({where: {id: id}});
+        let str = JSON.stringify(existingUser);
+        let par = JSON.parse(str);
+        if(!par){
+            msg = "User not Found";
+            res.status(500).json(exeption(msg));
+            return;
+        }
+        let obj = {
+        first_name: firstname,
+        last_name : lastname,
+        email : email,
+        phone : phone,
+        }
+
+        await UsersModel.update(obj,{where: {id: id}});
+         existingUser = await UsersModel.findOne({where: {id: id}});
+        return res.json(existingUser);
+    } catch (err) {
+        return res.status(500).json(err.message);
+    }
+}
+
+
+exports.deleteOne = async(id, res) => {
+
+    try{
+    let user = await UsersModel.findOne({where: {id: id}});
+    let userStr = JSON.stringify(user);
+    let userPar = JSON.parse(userStr);
+
+    if(!userPar){
+        msg = "User not found!"
+        return res.status(500).json(exeption(msg));
+    }
+
+    let deleted = await UsersModel.destroy({where: {id: id}});
+     return res.status(200).json(deleted);
+}catch(err){
+    res.status(500).json(err.message)
+}
+}
+
+exports.deleteAll = async(req, res) => {
+    try {
+        await UsersModel.findAll();
+      let deleted = await UsersModel.destroy({where:{}});
+      return res.status(200).json(deleted);
+    } catch (err) {
+        res.status(500).json(err.message);
+    }
+}
+
